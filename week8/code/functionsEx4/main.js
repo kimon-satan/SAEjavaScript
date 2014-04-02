514716025b1f3bdfcb524c35437f8f7c8ed913b8
kimon-satan <simon@simonkatan.co.uk>
2014-04-02 12:50:20 +0100

    week 10

---
+++ week8/code/functionsEx4/main.js
@@ -0,0 +1,14 @@
+var foo = function(){};
+
+foo.kung = "hiyaaaaa";
+
+console.log(foo.kung);
+
+
+
+//Experiment 1: turn this into a named function and print kung to the console from within the function.
+
+//Experiment 2: rename the function Foo. 
+//Use the this keyword to set the kung property 
+//instantiate a new version of the function called foo
+//check that foo.kung == "hiyaaaaa"; 
